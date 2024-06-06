92e062c6e990441e30c96002d4fe0dd12f358bb3
Joel Whitaker <joel@alphi.dev>
2024-06-06 16:02:08 +0100

    chore: bump libsql client to 0.6.2
    - reset deno.lock too

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@ export type {
   Transaction,
   TransactionMode,
   Value,
-} from "npm:@libsql/client@0.6.0/node";
+} from "npm:@libsql/client@0.6.2/node";
 
-export { createClient } from "npm:@libsql/client@0.6.0/node";
+export { createClient } from "npm:@libsql/client@0.6.2/node";
